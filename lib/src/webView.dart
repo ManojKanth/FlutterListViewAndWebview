e004f1633120ca6191212b1faae437f979125d90
Gitlab <manoj@arimaclanka.com>
2020-12-31 23:42:04 +0530

    code formatted

--- lib/src/webView.dart
+++ lib/src/webView.dart
@@ -12,24 +12,6 @@ class WebViewClass extends StatefulWidget {
 }
 
 class _WebViewClassState extends State<WebViewClass> {
-//     BannerAd bannerAd = BannerAd(
-//   adSlotId: "testw6vs28auh3", // This is test slot id for banner ad
-//   size: BannerAdSize.s320x50, // Banner size
-//   adParam: AdParam(), // Special request options
-//   bannerRefreshTime: 60, // Refresh time in seconds
-//   listener: (AdEvent event, {int errorCode}) { // Event listener
-//     print("Banner Ad event : $event");
-//     print("Banner Ad event errorCode: $errorCode");
-//   },
-  
-// );
-
-//  @override
-//   void didChangeDependencies() {
-//     super.didChangeDependencies();
-//     this.bannerAd.loadAd();
-//     this.bannerAd.show();
-//   }
   Completer<WebViewController> _controller = Completer<WebViewController>();
   @override
   Widget build(BuildContext context) {
